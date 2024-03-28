0cc004355bf46c81d07eb24cc495701c3e5da2e6
greg <greg.cusack@anza.xyz>
2024-03-28 18:08:28 +0000

    add generate faucet keypair

--- src/genesis.rs
+++ src/genesis.rs
@@ -1,5 +1,19 @@
-use std::path::PathBuf;
+use {
+    log::*,
+    rand::Rng,
+    solana_core::gen_keys::GenKeys,
+    solana_sdk::signature::{Keypair, write_keypair_file},
+    std::{
+        error::Error, 
+        path::PathBuf,
+        result::Result,
+    },
+};
 
+fn output_keypair(keypair: &Keypair, outfile: &str) -> Result<(), Box<dyn Error>> {
+    write_keypair_file(keypair, outfile)?;
+    Ok(())
+}
 pub struct Genesis {
     pub config_dir: PathBuf,
 }
@@ -12,5 +26,18 @@ impl Genesis {
         }
         std::fs::create_dir_all(&config_dir).unwrap();
         Genesis { config_dir }
+    }
+
+    pub fn generate_faucet(&self) -> Result<(), Box<dyn Error>> {
+        info!("generating faucet keypair");
+        let outfile = self.config_dir.join("faucet.json");
+        let seed: [u8; 32] = rand::thread_rng().gen();
+        let keypair = GenKeys::new(seed).gen_keypair();
+
+        if let Some(outfile) = outfile.to_str() {
+            output_keypair(&keypair, outfile)
+                .map_err(|err| format!("Unable to write {outfile}: {err}"))?;
+        }
+        Ok(())
     }
 }
