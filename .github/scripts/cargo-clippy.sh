3d322242940fca4cad5266364f350e1b4ebedd03
yihau <yihau.chen@icloud.com>
2024-04-05 03:01:45 +0800

    add clippy component

--- .github/scripts/cargo-clippy.sh
+++ .github/scripts/cargo-clippy.sh
@@ -7,4 +7,6 @@ repo_root=$(git rev-parse --show-toplevel)
 # shellcheck disable=SC1091
 source "$repo_root/.github/scripts/rust-version.sh" stable >/dev/null
 
+rustup component add clippy --toolchain="$rust_stable"
+
 cargo +"$rust_stable" clippy
