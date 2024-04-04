04770b52084e3580e385dd65711b6cfe6bfbeb68
yihau <yihau.chen@icloud.com>
2024-04-04 17:58:11 +0800

    init ci

---
+++ .github/scripts/cargo-test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+repo_root=$(git rev-parse --show-toplevel)
+
+# shellcheck disable=SC1091
+source "$repo_root/.github/scripts/rust-version.sh" stable >/dev/null
+
+cargo +"$rust_stable" test
