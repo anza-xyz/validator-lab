c93830496cb9b7b2f8ec72eaf832ea0e34788f11
Yihau Chen <yihau.chen@icloud.com>
2024-04-05 02:32:27 +0800

    ignore ed25519-dalek audit (#22)

--- .github/scripts/cargo-audit.sh
+++ .github/scripts/cargo-audit.sh
@@ -7,4 +7,9 @@ repo_root=$(git rev-parse --show-toplevel)
 # shellcheck disable=SC1091
 source "$repo_root/.github/scripts/rust-version.sh" stable >/dev/null
 
-cargo +"$rust_stable" audit
+cargo_audit_ignores=(
+  # ed25519-dalek
+  --ignore RUSTSEC-2022-0093
+)
+
+cargo +"$rust_stable" audit "${cargo_audit_ignores[@]}"
